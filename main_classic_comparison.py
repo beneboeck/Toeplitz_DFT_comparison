5a17a4029526784fdff9ec84887de6de581c70b1
beneboeck <63995517+beneboeck@users.noreply.github.com>
2022-09-09 10:32:01 +0200

    Update main_classic_comparison.py

--- main_classic_comparison.py
+++ main_classic_comparison.py
@@ -20,9 +20,9 @@ K_dic = {
 }
 
 IMPROVING = True
-N = 32
+N = 64
 K = K_dic[str(N)] * np.ones(N)
-csv_file = open('./Autoregressive_DIM32_RUNS500_r0_4.txt','w')
+csv_file = open('./Autoregressive_DIM64_RUNS500_r0_7.txt','w')
 #csv_file = open('./test.txt','w')
 csv_writer = csv.writer(csv_file)
 
@@ -1396,7 +1396,7 @@ constraints = generating_constraints(N)
 
 # MODEL
 #AUTOREGRESSIVES MODEL GAUS
-r = 0.4
+r = 0.7
 C = np.zeros((N,N))
 for i in range(N):
     for j in range(N):
