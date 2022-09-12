b4e0ec0971df7b1144b9f790f033744124154a0b
beneboeck <63995517+beneboeck@users.noreply.github.com>
2022-09-12 21:06:51 +0200

    Update main_classic_comparison.py

--- main_classic_comparison.py
+++ main_classic_comparison.py
@@ -22,7 +22,7 @@ K_dic = {
 IMPROVING = True
 N = 64
 K = K_dic[str(N)] * np.ones(N)
-csv_file = open('./Autoregressive_DIM64_RUNS30_r0_8.txt','w')
+csv_file = open('./Autoregressive_DIM64_RUNS30_H0_6.txt','w')
 #csv_file = open('./test.txt','w')
 csv_writer = csv.writer(csv_file)
 
@@ -1859,18 +1859,18 @@ constraints_Eig = generating_constraints_eig(N)
 
 # MODEL
 #AUTOREGRESSIVES MODEL GAUS
-r = 0.8
-C = np.zeros((N,N))
-for i in range(N):
-    for j in range(N):
-        C[i,j] = r**(np.abs(j-i))
-
-#Brownian Motion (see shrinkage estimator original paper)
-#H = 0.8
+#r = 0.8
 #C = np.zeros((N,N))
 #for i in range(N):
 #    for j in range(N):
-#        C[i,j] = 0.5 * ( (np.abs(j-i) + 1)**(2*H) - 2 * np.abs(j-i)**(2*H) + np.abs((np.abs(j-i) - 1))**(2*H) )
+#        C[i,j] = r**(np.abs(j-i))
+
+#Brownian Motion (see shrinkage estimator original paper)
+H = 0.6
+C = np.zeros((N,N))
+for i in range(N):
+    for j in range(N):
+        C[i,j] = 0.5 * ( (np.abs(j-i) + 1)**(2*H) - 2 * np.abs(j-i)**(2*H) + np.abs((np.abs(j-i) - 1))**(2*H) )
 
 
 
@@ -1879,7 +1879,7 @@ for i in range(N):
 N_SAMPLES = [4,8,16,32,64,128]
 #N_SAMPLES = [4,8]
 RUNS = 30
-print(f'r: {r}, N: {N}, RUNS: {RUNS}')
+print(f'H: {H}, N: {N}, RUNS: {RUNS}')
 MSE_sCov_n = []
 MSE_toeplitz_n = []
 MSE_OAS_n = []
